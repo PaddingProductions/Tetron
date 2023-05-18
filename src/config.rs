722824f0940210846905310d15693424f638c9f6
Shine Chang <shine00chang@gmail.com>
2023-05-18 09:10:10 -0700

    readme

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,10 @@
 use crate::evaluator::EvaluatorMode;
 
-
-
 #[derive(Clone)]
 pub struct Config {
     pub depth: u8,
     pub eval_mode: EvaluatorMode,
 }
-
 impl Config {
     pub fn new (depth: u8, eval_mode: EvaluatorMode) -> Self {
         Self {
